03d2abe12b52ddfe2620abbd9193e5f0458ee5be
sebas <srestrep74@eafit.edu.co>
2023-08-24 10:26:20 -0500

    Creacion del carousel

--- movie/views.py
+++ movie/views.py
@@ -5,11 +5,17 @@ from .models import Movie
 # Create your views here.
 
 def home(request):
-    searchTerm = request.GET.get('searchMovie')
-    context = {
-        'searchTerm' : searchTerm,
-    }
-    return render(request, 'home.html', context)
+    search_term = request.GET.get('searchMovie')
+    movies = Movie.objects.all()
+    if search_term:
+        movies = movies.filter(title__icontains=search_term)
+    else:
+        movies = Movie.objects.all()
+    return render(request, "home.html", {
+        "search_term": search_term,
+        "movies": movies,
+        "title": "Home"
+    })
 
 def about(request):
     return render(request, 'about.html')
